fa9917a7720dd0feca26d40d22cd4d54b87e78fa
JMI <ikolajm@gmail.com>
2022-01-04 12:12:42 -0500

    added procfile and necessary changes for heroku deploy

--- BlogAnywhere/settings.py
+++ BlogAnywhere/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-bloganywhere.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn BlogAnywhere.wsgi --log-file -
